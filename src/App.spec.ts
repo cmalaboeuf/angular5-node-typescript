8ee110e94cf90a61748747c23b4141ca360682ce
Christophe Malaboeuf <cmalaboeuf@octo.com>
2018-02-12 22:20:24 +0100

    First commit with typscript server runnable

---
+++ src/App.spec.ts
@@ -0,0 +1,12 @@
+
+import * as supertest from 'supertest'
+import app from './App'
+
+describe('App', () => {
+  it('works', () =>
+    supertest(app)
+      .get('/')
+      .expect('Content-Type', /json/)
+      .expect(200)
+  )
+})
